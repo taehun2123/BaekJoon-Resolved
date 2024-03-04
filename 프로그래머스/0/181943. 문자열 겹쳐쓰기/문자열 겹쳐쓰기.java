9482274f6df0e612dea081b2523dc7b4ebacb163
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-03-04 15:37:16 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 12.35 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,16 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        // Calculate the end index for the replacement
-        int end = s + overwrite_string.length();
-
-        // Ensure we don't exceed the original string's bounds
-        if (end > my_string.length()) {
-            end = my_string.length();
-        }
-
-        // Create the new string
-        String answer = my_string.substring(0, s) + overwrite_string + my_string.substring(end);
-
-        return answer;
+        String start = my_string.substring(0, s);
+        String end = my_string.substring(s + overwrite_string.length());
+        return start + overwrite_string + end;
     }
 }
