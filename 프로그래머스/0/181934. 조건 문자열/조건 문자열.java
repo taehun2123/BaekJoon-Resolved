b9cd35dfad84d3f58b766a155d7a75dca091d776
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-03-06 09:44:24 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        if(ineq.equals(">") && eq.equals("=")){
+        String operator = ineq.concat(eq);
+        if(operator.equals(">=")){
             return n>=m ? 1 : 0;
-        } else if(ineq.equals("<") && eq.equals("=")) {
+        } else if(operator.equals("<=")) {
             return n<=m ? 1 : 0;
-        } else if(ineq.equals(">") && eq.equals("!")){
+        } else if(operator.equals(">!")){
             return n>m ? 1 : 0;
         } else {
             return n<m ? 1 : 0; 
