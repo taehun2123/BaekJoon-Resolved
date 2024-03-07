a400787aa9033e4e224f61aee811f75269e44673
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-03-07 10:42:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.96 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 96.4 MB, 시간: 77.07 ms
+메모리: 95.3 MB, 시간: 64.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 04일 17:56:47
+2024년 03월 07일 10:42:19
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -6,17 +6,14 @@ class Solution {
         String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
         
-        // 각 참가자의 등장 횟수를 카운트합니다.
         for(String p : participant) {
             map.put(p, map.getOrDefault(p, 0) + 1);
         }
         
-        // 완주한 참가자의 등장 횟수를 감소시킵니다.
         for(String c : completion) {
             map.put(c, map.get(c) - 1);
         }
         
-        // 등장 횟수가 0보다 큰 참가자를 찾습니다.
         for(String key : map.keySet()) {
             if(map.get(key) > 0) {
                 answer = key;
