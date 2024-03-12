5b0d2d0c638179d4973ca331ad5d6afb0217eade
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-03-12 11:33:25 +0900

    [level 0] Title: 수 조작하기 2, Time: 634.08 ms, Memory: 389 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -2,23 +2,17 @@ import java.util.*;
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        ArrayList<Character> list = new ArrayList<>();
         for(int i = 0; i<numLog.length-1; i++){
             if(numLog[i]+1 == numLog[i+1]){
-                list.add('w');
+                answer += 'w';
             } else if(numLog[i]-1 == numLog[i+1]){
-                list.add('s');
+                answer += 's';
             } else if(numLog[i]+10 == numLog[i+1]){
-                list.add('d');
+                answer += 'd';
             } else {
-                list.add('a');
+                answer += 'a';
             }
         }
-        StringBuilder builder = new StringBuilder(list.size());
-        for(Character ch: list)
-        {
-            builder.append(ch);
-        }
-        return builder.toString();
+        return answer;
     }
 }
