2767a3c2b49c7108e9c0cbd343d4e269874da887
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-03-18 11:53:52 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 81.67 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -3,12 +3,12 @@ class Solution {
     public String solution(String my_string, int[][] queries) {
         for(int[] query: queries){
             String back = "";
-            for(int i = query[1]; i>=query[0]; i--){
+            for(int i = query[1]; i>=query[0]; i--){ //역순으로 String 변수에 삽입
                 back += my_string.charAt(i);
             }
-            String start = my_string.substring(0, query[0]);
-            String end = my_string.substring(query[1]+1);
-            my_string = start + back + end;
+            String start = my_string.substring(0, query[0]); //0 ~ query[0]까지 문자열
+            String end = my_string.substring(query[1]+1); // query[1] ~ 끝까지 문자열
+            my_string = start + back + end; // 결합
         }
         return my_string;
     }
