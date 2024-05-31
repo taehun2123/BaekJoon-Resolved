c4e0ae2b1658064f267098e3a569c240146eb403
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-05-31 09:24:08 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String sprout = """
+                     ,r'"7
+            r`-_   ,'  ,/
+             \\. ". L_r'
+               `~\\/
+                  |
+                  |
+                """;
+
+        System.out.println(sprout);
+    }
+}
