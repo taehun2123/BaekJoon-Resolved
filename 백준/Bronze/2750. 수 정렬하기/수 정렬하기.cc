bd0e4f734af3b34eca2ee6acd0052c4534f1b11a
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-05 10:13:55 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> box;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    while(n--){
+        int su;
+        cin >> su;
+        box.push_back(su);
+    }
+    sort(box.begin(),box.end());
+    for(auto &i : box){
+        cout << i << "\n";
+    }
+}
