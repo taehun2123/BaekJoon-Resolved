4ca7ca0bacdbafd514d8c580f41b50d4e4924eb9
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-05 23:58:55 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -13,11 +13,13 @@ int main() {
     }
     long long sum = 0;
     for(int i = 0; i<length; i++){
-        arrs[i] *= pow(31, i);
-        arrs[i] %= mod;
+        for(int k = 0; k<i; k++){
+            arrs[i] *= 31;
+            arrs[i] %= mod;
+        }
         sum += arrs[i];
     }
 
-    cout << sum;
+    cout << sum % mod;
     return 0;
 }
