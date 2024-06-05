876b81ea3f5fc1f6e05b7e16595857ac969f960e
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-05 23:47:16 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2212 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<long long> arrs;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int length;
+    string s;
+    long long mod = 1234567891;
+    cin >> length >> s;
+    for(auto c: s){
+        arrs.push_back((c - 'a')+1);
+    }
+    long long sum = 0;
+    for(int i = 0; i<length; i++){
+        arrs[i] *= pow(31, i);
+        arrs[i] %= mod;
+        sum += arrs[i];
+    }
+
+    cout << sum;
+    return 0;
+}
