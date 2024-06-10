5a45424b8a770250af3b069303d1c961a7d07e94
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-10 10:00:49 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+stack<pair<int,char>> listing;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string input;
+    cin >> input;
+    for(auto ch : input){
+        if (ch == '+' || ch == '-') {
+            while(!listing.empty() && listing.top().first >= 1){
+                cout << listing.top().second;
+                listing.pop();
+            }
+            listing.emplace(1, ch);
+        }
+        else if(ch == '*' || ch == '/'){
+            while(!listing.empty() && listing.top().first >= 2){
+                cout << listing.top().second;
+                listing.pop();
+            }
+            listing.emplace(2,ch);
+        }
+        else if(ch == '('){
+            listing.emplace(0,ch);
+        }
+        else if(ch == ')'){
+            while(!listing.empty() && listing.top().first != 0){
+                cout << listing.top().second;
+                listing.pop();
+            }
+            if (!listing.empty() && listing.top().first == 0) {
+                listing.pop();
+            }
+        } else if(isdigit(ch) || isalpha(ch)) {
+            cout << ch;
+        }
+    }
+    while(!listing.empty()){
+        cout << listing.top().second;
+        listing.pop();
+    }
+    return 0;
+}
