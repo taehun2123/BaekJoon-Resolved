923c5e44a6c19a7a344399dc767832aa5e822a9a
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-12 22:50:18 +0900

    [Silver II] Title: DFS와 BFS, Time: 8 ms, Memory: 6072 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -2,59 +2,59 @@
 using namespace std;
 
 int arr[1001][1001];
-int visited[1001];
+bool visited[1001];
 int N, M, V;
 
-/* DFS - 재귀(시스템 스택 사용) (후입선출 LIFO) */
-void DFS(int V)
-{
-    visited[V] = 1;   //시작점 방문기록
-    cout << V << " "; //방문한 노드 출력
+/* DFS */
+void DFS(int V) {
+    stack<int> st;
+    st.push(V);
 
-    for (int i = 1; i <= N; i++)
-    {
-        if (arr[V][i] == 1 && visited[i] == 0)
-        {
-            DFS(i); //스택에 i 넣는 셈
-        }
-        if (i == N)
-            return;
-    }
-}
+    while (!st.empty()) {
+        int next = st.top();
+        st.pop();
 
-/* BFS - 큐 사용 (선입선출 FIFO) */
-void BFS(int V)
-{
-    queue<int> q; //큐 생성
-    q.push(V);    //시작노드 큐에 넣음
+        if (!visited[next]) {
+            visited[next] = true;
+            cout << next << " ";  // 방문한 노드 출력
 
-    while (!q.empty())
-    {
-        int next = q.front(); 
-        visited[next] = 1;    
-        cout << next << " ";  //방문한 노드 출력
-        q.pop();              
-
-        //방문했던 노드와 가까운 노드 큐에 넣어줌
-        for (int i = 1; i <= N; i++)
-        {
-            if (arr[next][i] == 1 && visited[i] == 0)
-            {
-                q.push(i);
-                visited[i] = 1;
+            for (int i = N; i >= 1; i--) {
+                if (arr[next][i] == 1 && !visited[i]) {
+                    st.push(i); // 스택에 i
+                }
             }
         }
     }
-
 }
 
-int main()
-{
-    int u, v;
-    cin >> N >> M >> V;
+/* BFS */
+void BFS(int V){
+    queue<int> q; //큐 생성
+    q.push(V);    //시작노드 큐에 넣음
 
-    for (int i = 0; i < M; i++)
-    {
+    while (!q.empty()){
+        int next = q.front();
+        q.pop();
+        if (!visited[next]) {
+            visited[next] = true;
+            cout << next << " ";  // 방문한 노드 출력
+
+            for (int i = 1; i <= N; i++) {
+                if (arr[next][i] == 1 && !visited[i]) {
+                    q.push(i); // 스택에 i
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int u, v;
+    cin >> N >> M >> V; //N - 정점의 개수, M - 간선의 개수, V - 방문하는 점.
+
+    for (int i = 0; i < M; i++){
         cin >> u >> v;
         arr[u][v] = 1;
         arr[v][u] = 1; //자리바꿔서도 해주는 이유 : 무방향이기 때문
@@ -63,7 +63,7 @@ int main()
     DFS(V); // DFS 수행
 
     cout << "\n";
-    fill(visited, visited + 1001, 0); //방문기록 visited 초기화
+    fill(visited, visited + 1001, false); //방문기록 visited 초기화
 
     BFS(V); // BFS 수행
 }
