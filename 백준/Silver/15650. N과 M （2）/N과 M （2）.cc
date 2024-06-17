a19aa5ee33b38bdece15be5114fae09004c60a15
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-17 16:33:32 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[10];
+bool isUsed[10];
+int n, m;
+void solved(int k){
+    if(k == m){
+        for(int i = 0; i<m; i++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i<=n; i++){
+        if(!isUsed[i] && arr[k-1] < i){
+            arr[k] = i;
+            isUsed[i] = 1;
+            solved(k+1);
+            isUsed[i] = 0;
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    solved(0);
+    return 0;
+}
+
