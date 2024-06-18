e39a6fa1a4c5d1809f45f2e565e03d9aaeafdd0c
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-19 01:48:18 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ans = 0;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  while (n--) {
+    string a;
+    cin >> a;
+    stack<int> S;
+    for (auto c : a) {
+      if (!S.empty() && S.top() == c) S.pop();
+      else S.push(c);
+    }
+    if (S.empty()) ans++;
+  }
+  cout << ans << "\n";
+}
