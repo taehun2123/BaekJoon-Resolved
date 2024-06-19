06aedcab235035dd7f046e5e8ceae34c2e73c407
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-19 22:23:45 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string input;
+    cin >> input;
+    cout << ":fan::fan::fan:" <<"\n";
+    cout << ":fan::" << input << "::fan:" <<"\n";
+    cout << ":fan::fan::fan:" <<"\n";
+}
