dc99dce92c22825fba00196bd8669e5e5fe36aab
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-24 11:24:08 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,58 +1,52 @@
 #include <bits/stdc++.h>
-using namespace std;
-
 #define X first
 #define Y second
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int m, n, k;
+using namespace std;
 int board[102][102];
 int vis[102][102];
-
-int main(){
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int m, n, k, cnt;
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     cin >> m >> n >> k;
-    while (k--) {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-        for (int j = y1; j < y2; j++)
-            for (int k = x1; k < x2; k++)
-                board[j][k] = 1;
+    while(k--){ // 벽 세우기 작업
+        int spx, spy, epx, epy;
+        cin >> spx >> spy >> epx >> epy;
+        for(int j = spy; j<epy; j++)
+            for(int l = spx; l<epx; l++)
+                board[j][l] = 1;
     }
-
-    int count = 0;
-    vector<int> ans;
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (board[i][j] == 1 || vis[i][j] == 1) continue;
-            queue<pair<int, int>> Q;
+    vector<int> arrNub;
+    for(int i = 0; i<m; i++){
+        for(int j = 0; j<n; j++){
+            if(board[i][j] == 1 || vis[i][j] == 1) continue;
+            queue<pair<int,int>> Q;
             vis[i][j] = 1;
-            Q.push({ i, j });
-            int width = 1;
-            count++;
-            while (!Q.empty()) {
+            int nub = 1;
+            cnt++;
+            Q.push({i, j});
+            while(!Q.empty()){
                 auto cur = Q.front(); Q.pop();
-                for (int dir = 0; dir < 4; dir++) {
+                for(int dir = 0; dir<4; dir++){
                     int nx = cur.X + dx[dir];
                     int ny = cur.Y + dy[dir];
-                    if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-                    if (board[nx][ny] == 1 || vis[nx][ny] == 1) continue;
-                    Q.push({ nx, ny });
+                    if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                    if(board[nx][ny] == 1 || vis[nx][ny] == 1) continue;
                     vis[nx][ny] = 1;
-                    width++;
+                    Q.push({nx, ny});
+                    nub++;
                 }
             }
-            ans.push_back(width);
+            arrNub.push_back(nub);
         }
     }
-    sort(ans.begin(), ans.end());
-
-    cout << count << '\n';
-    for (int i : ans)
-        cout << i << ' ';
+    cout << cnt <<"\n";
+    sort(arrNub.begin(), arrNub.end());
+    for(auto i:arrNub){
+        cout << i << " ";
+    }
 
     return 0;
 }
