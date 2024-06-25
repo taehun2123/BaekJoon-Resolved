48d6151e5d6f9ba1cbeb9ee3eadc4d63058ea68b
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-06-25 17:00:23 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[10];
+int init[10];
+bool isUsed[10];
+int n, m;
+void solved(int k){
+    if(k == m){
+        for(int i = 0; i<m; i++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    int i = 0;
+    while(init[i] != 0){
+        if(!isUsed[i] && init[i] > arr[k-1]){
+            arr[k] = init[i];
+            isUsed[i] = 1;
+            solved(k+1);
+            isUsed[i] = 0;
+        }
+        i++;
+    }
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for(int i = 0; i<n; i++){
+       cin >> init[i];
+    }
+    for(int i = 0; i<n; i++){
+        for(int j = i; j<n; j++){
+            if(init[i] > init[j]){
+                int temp = init[i];
+                init[i] = init[j];
+                init[j] = temp;
+            }
+        }
+    }
+    solved(0);
+    return 0;
+}
