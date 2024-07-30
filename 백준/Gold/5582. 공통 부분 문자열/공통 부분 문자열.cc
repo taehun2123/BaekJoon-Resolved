b91da689a5793c9923812da7ce244d969b90991d
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-07-30 10:39:00 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 64 ms, Memory: 64588 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+string firstStr;
+string secondStr;
+int ans;
+int dp[4002][4002];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> firstStr >> secondStr;
+    for(int i = 1; i <= firstStr.size(); i++){
+        for(int j = 1; j<=secondStr.size(); j++){
+            if(firstStr[i-1] == secondStr[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+            else dp[i][j] = 0;
+            ans = max(ans, dp[i][j]);
+            //부분 수열은 연속된 값이 아니므로, 이전의 최대 공통부분 수열을 가져오기 위함
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
