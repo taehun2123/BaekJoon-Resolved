7dd1dad1f676c65c3e28eb46a13bef7341f0ac08
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-08-07 13:10:12 +0900

    [Gold II] Title: 문제집, Time: 24 ms, Memory: 3964 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj[32002];
+int deg[32002];
+int n,m;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    while(m--){
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        deg[b]++;
+    }
+
+    priority_queue<int, vector<int>, greater<>> Q;
+    for(int i = 1; i<=n; i++) {
+        if(deg[i] == 0) Q.push(i);
+    }
+    while (!Q.empty()){
+        auto cur = Q.top(); Q.pop();
+        cout << cur << " ";
+        for(int nxt : adj[cur]){
+            if(--deg[nxt] == 0) Q.push(nxt);
+        }
+    }
+    return 0;
+}
