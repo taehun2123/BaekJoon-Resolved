b385fe296e1013546093317465c9a004822f50a1
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-08-19 13:56:36 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cin.tie(0);
+    char a;
+    cin >> a;
+    if(a == 'N' || a == 'n') cout << "Naver D2";
+    else cout << "Naver Whale";
+    return 0;
+}
