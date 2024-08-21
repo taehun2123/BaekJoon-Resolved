16e42df6ba3acc5b72ef874d92032116983e0c0e
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-08-21 09:37:48 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long a, b;
+    cin >> a >> b;
+    if(a == b) cout << 1;
+    else cout << 0;
+    return 0;
+}
