cf5e0fc65a56ced1959cd0306918e8ba7269280b
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-09-04 09:15:05 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "  ___  ___  ___\n";
+  cout << "  | |__| |__| |\n";
+  cout << "  |           |\n";
+  cout << "   \\_________/\n";
+  cout << "    \\_______/\n";
+  cout << "     |     |\n";
+  cout << "     |     |\n";
+  cout << "     |     |\n";
+  cout << "     |     |\n";
+  cout << "     |_____|\n";
+  cout << "  __/       \\__\n";
+  cout << " /             \\\n";
+  cout << "/_______________\\\n";
+
+  return 0;
+}
