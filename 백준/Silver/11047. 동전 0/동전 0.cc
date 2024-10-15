ef8961ea5db0f10740c85d54bc74ad30e9b11306
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-10-15 11:13:39 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,18 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+int N, K, cnt;
+int arr[11];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> K;
+    for(int i = 0; i<N; i++)cin >> arr[i];
+    for(int i = N-1; i>=0; i--){
+        if(K % arr[i] == 0){
+            cnt += K/arr[i];
+            K%=arr[i];
+            break;
+        }
+        if(K % arr[i] != K) {
+            cnt += K / arr[i];
+            K%=arr[i];
+        }
+    }
 
-int n, k;
-int a[15];
+    cout << cnt;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int ans = 0;
-  cin >> n >> k;
-  for(int i = 0; i < n; i++) cin >> a[i];
-  for(int i = n-1; i >= 0; i--){
-    ans += k / a[i];
-    k %= a[i];
-  }
-  cout << ans;
+    return 0;
 }
