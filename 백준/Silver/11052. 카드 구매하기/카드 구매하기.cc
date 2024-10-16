826c2f460e455877ad33fe4799d7ea70ee06f81a
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-10-16 15:55:21 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * 카드 N개를 갖기위해 지불해야 하는 금액의 최댓값
+ */
+int dp[1001];
+int card[1001];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N; // 카드의 종류 n가지
+    cin >> N;
+    for(int i = 1; i<=N; i++) cin >> card[i];
+    for(int i = 1; i<=N; i++){
+        for(int j = 1; j<=i; j++){
+            dp[i] = max(dp[i], dp[i-j] + card[j]);
+        }
+    }
+
+    cout << dp[N];
+    return 0;
+}
