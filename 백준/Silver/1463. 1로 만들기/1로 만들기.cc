af689ca52355d15dd89597f2e496551cf3bad0f2
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-10-16 14:51:55 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int box[1000001];
+int dp[1000001]; //횟수의 최솟값;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int x;
-    cin >> x;
-    box[1] = 0;
-    box[2] = 1;
-    box[3] = 1;
-    for(int i = 4; i<=x; i++){
-        box[i] = box[i-1] + 1;
-        if(i%2==0) box[i] = min(box[i], box[i/2]+1);
-        if(i%3==0) box[i] = min(box[i], box[i/3]+1);
+    dp[0] = dp[1] = 0;
+    dp[2] = dp[3] = 1;
+    int N;
+    cin >> N;
+    for(int i = 4; i<=N; i++){
+        dp[i] = dp[i-1] + 1;
+        if(i%2 == 0) dp[i] = min(dp[i/2] + 1, dp[i]);
+        if(i%3 == 0) dp[i] = min(dp[i/3] + 1, dp[i]);
     }
-    cout << box[x];
+
+    cout << dp[N];
+
     return 0;
 }
