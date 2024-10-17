b82b7a7e74fcd526d694bddce53eb1049db9676f
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-10-17 15:33:13 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-int r[1002], b[1002], g[1002];
-int paint[1002][3];
+int dp[1002][4]; //i번째 집까지 칠할때의 비용 최솟값, j는 색 0 = R 1 = G 2= B
+int R[1002];
+int G[1002];
+int B[1002];
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> n;
-    for(int i=1; i<=n; i++) cin >> r[i] >> g[i] >> b[i];
-    paint[1][0] = r[1]; //첫 번째 집이 빨강일 때
-    paint[1][1] = g[1]; // 첫 번째 집이 초록일 때
-    paint[1][2] = b[1]; // 첫 번째 집이 파랑일 때
-    for(int i = 2; i<=n; i++){
-        paint[i][0] = min(paint[i-1][1], paint[i-1][2]) + r[i]; //i번째 집이 빨강일때 최소값
-        paint[i][1] = min(paint[i-1][0], paint[i-1][2]) + g[i];
-        paint[i][2] = min(paint[i-1][0], paint[i-1][1]) + b[i];
+    int N;
+    cin >> N;
+    for(int i = 1; i<=N; i++) cin >> R[i] >> G[i] >> B[i];
+
+    dp[1][0] = R[1];
+    dp[1][1] = G[1];
+    dp[1][2] = B[1];
+
+    for(int i = 2; i<=N; i++){
+        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + R[i];
+        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + G[i];
+        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + B[i];
     }
-    cout << min({paint[n][0], paint[n][1], paint[n][2]});
+
+    cout << *min_element(dp[N], dp[N]+3);
     return 0;
 }
