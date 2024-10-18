b6c159b1991642c677d3fd37426d2a7a378185b7
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-10-18 10:44:04 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,69 +1,48 @@
 #include <bits/stdc++.h>
+#define pii pair<int,int>
 using namespace std;
-
-int arr[1001][1001];
-bool visited[1001];
 int N, M, V;
+bool vis[1002];
+vector<int> adj[1002];
 
-/* DFS */
-void DFS(int V) {
-    stack<int> st;
-    st.push(V);
-
-    while (!st.empty()) {
-        int next = st.top();
-        st.pop();
-
-        if (!visited[next]) {
-            visited[next] = true;
-            cout << next << " ";  // 방문한 노드 출력
-
-            for (int i = N; i >= 1; i--) {
-                if (arr[next][i] == 1 && !visited[i]) {
-                    st.push(i); // 스택에 i
-                }
+void DFS(int x){
+    cout << x << " ";
+    vis[x] = true;
+    sort(adj[x].begin(), adj[x].end());
+    for(int y : adj[x])
+        if(!vis[y]) DFS(y);
+}
+void BFS(int st){
+    queue<int> Q;
+    Q.push(st);
+    vis[st] = true;
+    while(!Q.empty()){
+        int x = Q.front(); Q.pop();
+        cout << x << " ";
+        for(int i = 0; i<adj[x].size(); i++){
+            sort(adj[x].begin(), adj[x].end());
+            int y = adj[x][i];
+            if(!vis[y]){
+                Q.push(y);
+                vis[y] = true;
             }
         }
     }
 }
-
-/* BFS */
-void BFS(int V){
-    queue<int> q; //큐 생성
-    q.push(V);    //시작노드 큐에 넣음
-
-    while (!q.empty()){
-        int next = q.front();
-        q.pop();
-        if (!visited[next]) {
-            visited[next] = true;
-            cout << next << " ";  // 방문한 노드 출력
-
-            for (int i = 1; i <= N; i++) {
-                if (arr[next][i] == 1 && !visited[i]) {
-                    q.push(i); // 스택에 i
-                }
-            }
-        }
-    }
-}
-
-int main(){
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int u, v;
-    cin >> N >> M >> V; //N - 정점의 개수, M - 간선의 개수, V - 방문하는 점.
-
-    for (int i = 0; i < M; i++){
+    cin >> N >> M >> V;
+    for(int i = 1; i<=M; i++){
+        int u, v;
         cin >> u >> v;
-        arr[u][v] = 1;
-        arr[v][u] = 1; //자리바꿔서도 해주는 이유 : 무방향이기 때문
-    }                  //입력 즉시 인접행렬에 넣어줌, 다 돌면 인접행렬 완성
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
 
-    DFS(V); // DFS 수행
-
+    DFS(V);
     cout << "\n";
-    fill(visited, visited + 1001, false); //방문기록 visited 초기화
-
-    BFS(V); // BFS 수행
+    memset(vis, 0, sizeof(vis));
+    BFS(V);
+    return 0;
 }
