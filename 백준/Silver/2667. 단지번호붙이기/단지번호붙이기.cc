7920db254a74095525dc6d0b790d082c51cea92a
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-10-18 12:13:30 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,49 +1,49 @@
 #include <bits/stdc++.h>
-#define X first
-#define Y second
 using namespace std;
 string board[30];
-int vis[30][30];
-int n;
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+int nx[4] = {0, 1, 0, -1};
+int ny[4] = {1, 0, -1, 0};
+bool dist[30][30];
+queue<pair<int,int>> Q;
+vector<int> house;
+int N, ans;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> n;
-    for(int i = 0; i<n; i++){
+    cin >> N;
+
+    //보드 그리기
+    for (int i = 0; i < N; i++) {
         cin >> board[i];
     }
-    int sum = 0;
-    vector<int> arr;
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){
-            if(board[i][j] == '0' || vis[i][j] == 1) continue;
-            queue<pair<int,int>> Q;
+
+    //탐색
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++) {
+            if (board[i][j] != '1' || dist[i][j]) continue;
+            ans++;
+            int size = 1;
+            dist[i][j] = 1;
             Q.push({i, j});
-            vis[i][j] = 1;
-            sum++;
-            int cnt = 0;
-            while(!Q.empty()){
+            while (!Q.empty()) {
                 auto cur = Q.front(); Q.pop();
-                cnt++;
-                for(int i = 0; i<4; i++){
-                    int nx = cur.X + dx[i];
-                    int ny = cur.Y + dy[i];
-                    if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                    if(board[nx][ny] == '0' || vis[nx][ny] == 1) continue;
-                    vis[nx][ny] = 1;
-                    Q.push({nx, ny});
+                size++;
+                for (int k = 0; k < 4; k++) {
+                    int nfx = cur.first + nx[k];
+                    int nfy = cur.second + ny[k];
+                    if (nfx < 0 || nfx >= N || nfy < 0 || nfy >= N) continue;
+                    if (dist[nfx][nfy] || board[nfx][nfy] != '1') continue;
+                    dist[nfx][nfy] = 1;
+                    Q.push({nfx, nfy});
                 }
             }
-            arr.push_back(cnt);
+            house.push_back(size);
         }
     }
-    sort(arr.begin(), arr.end());
-    cout << sum << "\n";
-    for(auto i : arr){
-        cout << i << "\n";
+    sort(house.begin(), house.end());
+    cout << ans << "\n";
+    for(auto x: house){
+        cout << x-1 << "\n";
     }
-
     return 0;
 }
