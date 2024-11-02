5c270a3027f738ebc49ceeda4c61ff0a1d622aa3
taehun2 <132832057+taehun2123@users.noreply.github.com>
2024-11-02 23:55:47 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.cc
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dp[1002][10]; // a자리에서 b가 마지막 수로 왔을 때의 오르막수의 개수
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n;
-    cin >> n;
-    for(int i = 0 ;i<10; i++) dp[1][i] = 1;
+#define MOD 10007
 
-    for(int i = 2; i<=n; i++)
-        for(int j = 0; j<10; j++)
-            for(int k = 0; k<=j; k++){
-                dp[i][j] += dp[i-1][k];
-                dp[i][j] %= 10007;
-            }
+int sol(int n) {
+    int dp[10];
+    fill(dp, dp + 10, 1);
 
+    for(int i = 1; i < n; i++) {
+        // 기존 배열을 누적합으로 바꾼다.
+        // 우리가 알고자 하는 것은 N = n일때의 배열의 전체 합이기 때문에
+        // 중간 배열을 저장할 필요도 없고, 저장해둘 필요도 없다.
+        for(int j = 0; j < 9; j++) {
+            dp[j + 1] += dp[j] % MOD;
+            dp[j + 1] %= MOD;
+        }
+    }
+    return accumulate(dp, dp + 10, 0) % MOD;
+}
 
-    int ans=0;
-    for(int i = 0; i<10; i++) ans += dp[n][i];
-
-    cout << ans % 10007;
-
-    return 0;
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin >> n;
+    cout << sol(n);
 }
