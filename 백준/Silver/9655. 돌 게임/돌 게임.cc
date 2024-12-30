533b0a7d1d827a9044fc19e2a2981e2b94201efc
DEVH <132832057+taehun2123@users.noreply.github.com>
2024-12-30 21:55:35 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int main() {
+    ios::sync_with_stdio(0);
+    cin >> N;
+    if(N%2 == 0) cout << "CY";
+    else cout << "SK";
+    return 0;
+}
