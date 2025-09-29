6505bb4c65ccb962534363718f2e87f8e4b5534e
DEVH <132832057+taehun2123@users.noreply.github.com>
2025-09-29 18:51:24 +0900

    [Silver III] Title: 소수 구하기, Time: 8 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[1000001];
+int n, m;
+vector<bool> isPrime(1000001, true);
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n, m;
-    int num = 2;
-    arr[0] = 1;
-    arr[1] = 1;
+    cin.tie(0)->sync_with_stdio(0);
     cin >> n >> m;
-    while(num <= m){ // 모든 제곱수는 소수에서 제외하기
-        for(int i = 2; i*num <= m; i++)
-                arr[num*i] = 1;
-        num++;
+    isPrime[1] = false;
+    for(int i = 2; i*i <= m; i++){
+        if(!isPrime[i]) continue;
+        for(int j = i*i; j<=m; j+=i){
+            isPrime[j] = false;
+        }
     }
-    for(int i = n; i<= m; i++)
-            if(!arr[i]) cout << i << "\n";
+    for(int i = n; i<=m; i++){
+        if(isPrime[i]) cout << i << "\n";
+    }
+
+    return 0;
 }
