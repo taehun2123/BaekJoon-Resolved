90bc353646c5f6d3047630b63cff3e56a6e45e4c
DEVH <132832057+taehun2123@users.noreply.github.com>
2025-11-08 20:49:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+long long a;
+long long b;
+long long c;
+
+int main() {
+    cin >> a >> b >> c;
+    long long sum = 0;
+    sum = a + b + c;
+    cout << sum;
+    return 0;
+}
